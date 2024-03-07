72a9dcdc7dbdb2ecc83093865765a85527bcde79
Rakesh <rakeshdeveloper23@gmail.com>
2024-03-07 17:05:52 +0530

    aded

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -b 0.0.0.0:8000 run:__init__
+web: gunicorn -w 4 -b 0.0.0.0:8000 run:app
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from deyyam.__init__ import create_app
+from deyyam.app import create_app
 
 app=create_app()
 
